e07c85448a972a4f93a6ff29a0229b0a019a0ef8
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2024-04-19 11:39:30 -0700

    Implement user and file input
    Author: dangkyle64
    Date: 4/19/2024 1134
    
    Implement choice between user and file input for more options (currently only input.txt is our input file)
    
    Loop the request until the user is happy to make sure the credits used in the API are used without mistakes
    
    Implement check to make sure the user selects the proper choice between the two options, cancels the program if they do not

--- extract.py
+++ extract.py
@@ -6,15 +6,31 @@ class extract_:
     """
     Plan to extract the text here to make it easier for the API to read and to streamline requests into one method 
     """
-    def extract_words():
-
+    def extract_words(self):
+        text = ""
         #implement a selection option between file reading and text reading
+        stop = "continue"
+        choice = input("Select input: file or user -> ")
         
-        #Read text data from file
-        with open ('input.txt', 'r', encoding = 'utf-8') as file:
-            text = file.read()
 
-        #implement text reading 
+        #Read text data from file
+        while stop == "continue":
+            
+            #file input here
+            if choice == "file":
+                with open ("input.txt", "r", encoding = "utf-8") as file:
+                    text = file.read()
+                stop = input("If you wish to stop, put any other input than continue: ")
+
+            #user input here
+            elif choice == "user":
+                text = input ("Please input a sentence: ")
+                stop = input("If you wish to stop, put any other input than continue: ")
+
+            #check if other choices are attempted after deciding to input
+            else:
+                print(f"This is not a valid choice between file or user")
+                stop = "stopped"
 
         #return as a string
         return text
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from sentiment_analysis import sentiment
 def main():
     #extract text from file input / user input
     extract_text = extract_()
-
+    text = extract_text.extract_words()
+    
     #initalize sentiment analysis object with extracted text
-    sentiment_analyze = sentiment(extract_text)
+    sentiment_analyze = sentiment(text)
 
     sentiment_analyze.sentiment_input()
     
