57eac819d187d47f440d4d30ac379851c340324e
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2024-04-18 18:53:56 -0700

    implement file input.txt for API reading
    author: dangkyle64
    date 4/18/2024 1852
    
    Included input.txt for user input being sent into the API for sentiment analysis.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,14 +1,28 @@
 import requests 
 
+#API Endpoint
 url = 'https://api.meaningcloud.com/sentiment-2.1'
 
+#Payload parameters 
+
 payload = {
-    'key': 'put code here',
-    'txt':'',
+    'key': 'input key here',
     'lang': 'en',
+
 }
 
-response = requests.post(url, data = payload)
+#Read text data from file
+with open ('input.txt', 'r', encoding = 'utf-8') as file:
+    text = file.read()
+
+#Include the 'txt' into the payload to be sent to the endpoint
+payload['txt'] = text
+
+#Define file input
+files = {}
+
+#Request to the API here
+response = requests.post(url, data = payload, files = files)
 
 print(f'Status code: {response.status_code}')
 print(response.json())
