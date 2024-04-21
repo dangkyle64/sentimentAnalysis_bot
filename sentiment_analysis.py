40fdc65c5e95c7b29c634d06c25b49b63a500a7e
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2024-04-21 09:28:53 -0700

    Add pulling of specific keys from .json output
    Author: dangkyle64
    Date: 4/21/2024 0927

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,13 +6,13 @@ url = 'https://api.meaningcloud.com/sentiment-2.1'
 class sentiment:
 
     def __init__(self, text):
-        self.text = ''
+        self.text = text
 
     def sentiment_input(self):
         #Payload parameters 
 
         payload = {
-            'key': 'input key here',
+            'key': '',
             'lang': 'en',
 
         }
@@ -26,7 +26,26 @@ class sentiment:
         #Request to the API here
         response = requests.post(url, data = payload, files = files)
 
-        print(f'Status code: {response.status_code}')
-        print(response.json())
+        #Store .json file for use
+        data = response.json()
 
+        #print(f'Status code: {response.status_code}')
+        #print(response.json())
+
+        #Using specific keys to take out information 
+        score = data["score_tag"]
+        confidence = data["confidence"]
+
+        #Access list from nested key in .json file
+        sentence_list = data["sentence_list"]
+
+        #Loop through each sentence dictionary
+        for sentence in sentence_list:
+            #Access score_tag, confidence, and text keys inside of sentence for use
+            score2 = sentence["score_tag"]
+            confidence2 = sentence["confidence"]
+            input_statement = sentence["text"]
+
+        print (f"The total sentiment score is {score} with a confidence of {confidence}")
+        print (f"Specifically, input: {input_statement}. This has a score of {score2} and confidence of {confidence2}")
         return 0
