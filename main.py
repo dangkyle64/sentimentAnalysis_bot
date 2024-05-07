fc2c044ee360217a0921bc428b4a07c7fbcf36d3
zacahryy <zachfaulkner02@gmail.com>
2024-05-06 19:37:59 -0700

    mongodb updates

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from extract import extract_
 from sentiment_analysis import sentiment
+from mongo import mongo_
 
 def main():
     #extract text from file input / user input
+    #mongo_.insertData("now", "working")
     extract_text = extract_()
     text = extract_text.extract_words()
+    
     
     #initalize sentiment analysis object with extracted text
     sentiment_analyze = sentiment(text)
 
-    sentiment_analyze.sentiment_input()
+    output = sentiment_analyze.sentiment_input()
+    mongo_.insertData(text, output)
+    
     
     return 0
 
--- mongo.py
+++ mongo.py
@@ -1,16 +1,20 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://zachfaulkner02:$Greinke21@cluster0.x5robxp.mongodb.net/")
+class mongo_():
+    def insertData(textInput, textOutput):
+        db = client["db"]
+        collection = db["data"]
+        doc = {"input": textInput, "output": textOutput}
+        collection.insert_one(doc)
 
-db = client["db"]
+    
 
-collection = db["tuples"]
 
-rec={
 
-}
 
-rec = db.myTable.insert(rec)
+
+
 
 
 
